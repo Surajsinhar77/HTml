b4383aab1e30713d9718c1af6451b40524f04452
Suraj <vikeesinha27@gmail.com>
2023-02-07 19:33:23 +0530

    addingJS

--- CSS/Js/index.js
+++ CSS/Js/index.js
@@ -58,13 +58,13 @@
 // false = true;
 
 
-let age = 19
+// let age = 19
 
-if( true || false){
-    console.log(" i am coming");
-}else{
-    console.log(" now it my turn")
-}
+// if( true || false){
+//     console.log(" i am coming");
+// }else{
+//     console.log(" now it my turn")
+// }
 
 
 // if(true){
@@ -85,3 +85,121 @@ if( true || false){
 
 
 // console.log(a, A);
+
+
+// ---------------------------------------------------------
+
+// let age = 50;
+
+// if(age<=18){
+//     console.log("child")
+// }else if(age>=100){
+//     console.log("enter write input");
+// }else{
+//     console.log("adult")
+// }
+
+
+// ---------------------------------------------------------
+// let a = 2;
+
+// nested condition  2 > 0
+
+// if(a > 0){
+//     if(a%2 == 0){   //2%2 == 0
+//         console.log("a is positive even no")
+//     }else{
+//         console.log(" a is positive odd no")
+//     }
+// }else{
+//     if(a%2 == 0){
+//         console.log("a is negative even no")
+//     }else{
+//         console.log(" a is negative odd no")
+//     }
+// }
+
+// while, for , do while
+
+// while(){
+
+// }
+
+// console.log("1 ,")
+// let a = 1;
+
+// while(true){   // 2<10    10<10
+//     console.log(a);
+//     a = a+1;  // a =  1+1 // 2
+// }
+
+
+// let y = 0;
+// while(y < 5){
+//     console.log("Suraj Kumar")
+//     y++;
+// }
+
+// *****
+// *****
+// *****
+// *****
+
+
+
+// let a = 'C' 
+
+// switch(a) {
+//     case 'A':
+//         console.log("i got A")
+//         break;
+//     case 'B':
+//         console.log("i got B");
+//         break;
+//     case 'C': 
+//         console.log("i got C")
+//         break;
+//     default:
+//         console.log("i am other then A ")
+//         break;
+// }
+
+// console.log(" here i came")
+
+
+// let a = 12
+// let i = 1;
+// while(i<=10){
+//     console.log(a , "*" , i , " = ", a*i);
+//     i++; // i = i+1  // 2 * 1 = 2  // 2 * 2 = 4
+// }
+// console.log(" 2nd table : ")
+
+// let b = 6
+// while(i<=10){
+//     console.log(b , "*" , i , " = ", b*i);
+//     i++; // i = i+1  // 2 * 1 = 2  // 2 * 2 = 4
+// }
+
+
+
+
+// let a =7;
+// for(let i = 1; i<=10; i++){
+//     console.log(a , "*" , i , " = ", a*i);
+// }
+
+// console.log(" 2nd table : ")
+
+// let b = 6
+// for(let i = 1; i<=10; i++){
+//     console.log(b , "*" , i , " = ", b*i);
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+
+
